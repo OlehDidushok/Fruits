171fc7f693d618ab28c73f3a6c349aa2e369d758
“Oleh <o.didushok@lampalampa.net>
2024-06-09 14:48:31 +0300

    Set up op project

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Oleh on 09.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
