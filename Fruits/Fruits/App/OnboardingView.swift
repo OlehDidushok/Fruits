2428d0719172db51c658275e49970bb9aa4f5046
“Oleh <o.didushok@lampalampa.net>
2024-06-14 23:02:01 +0300

    Added locale dataModel

--- Fruits/Fruits/App/OnboardingView.swift
+++ Fruits/Fruits/App/OnboardingView.swift
@@ -9,15 +9,15 @@ import SwiftUI
 
 struct OnboardingView: View {
     // MARK: - PROPERTIES
+    var fruits: [Fruit] = fruitsData
     
     //MARK: - BODY
     
     var body: some View {
         TabView {
-            ForEach(0..<5) { item in
-                FruitCardView()
-            }
-           
+            ForEach(fruits[0...5]) { item in
+                FruitCardView(fruit: item)
+            } //: LOOP
         } //: TAB
         .tabViewStyle(PageTabViewStyle())
         .padding(.vertical, 20)
